872bb50320c94630c64e259f6c8c26b18c41b25c
OmerKhan-88 <73921844+OmerKhan-88@users.noreply.github.com>
2022-03-09 00:26:19 -0700

    Add files via upload

---
+++ appStart.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdbms;
+
+/**
+ *
+ * @author Omer Khan
+ */
+public class appStart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        UserSelection start = new UserSelection();
+        start.show();
+    }
+    
+}
